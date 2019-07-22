57a444e48281ed1948697de962ae4c4dd56af5f7
sooyeon <tndus130@ajou.ac.kr>
2019-07-23 00:22:12 +0900

    Button

---
+++ App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button, StyleSheet, View } from 'react-native';
+
+export default class App extends React.Component {
+  render(){
+   return (
+    <View style={styles.container}>
+      <Button title={'click me!'} onPress={() => console.log('Clicked!')}/>
+    </View>
+   );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+ // textBig: {
+ //   fontSize:32,
+ // },
+ // textRed: {
+ //   color: 'red'
+//  },
+});
