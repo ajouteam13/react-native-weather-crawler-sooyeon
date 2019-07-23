bc5d733b27dea1a8e21ad7548baf7d2b540c94ee
sooyeon <tndus130@ajou.ac.kr>
2019-07-23 14:46:41 +0900

    Seperate components

--- App.js
+++ App.js
@@ -31,46 +31,13 @@ class CounterButton extends React.Component {
   }
 }
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      cities: [],
-    };
-  }
-
-  componentDidMount() {
-    fetch('http://demo6468405.mockable.io/weather-crawlers/cities')
-      .then(response => response.json())
-      .then(cities => {
-        console.log('cities =', cities.length);
-        this.setState({
-          cities
-        });
-      });
-  }
-
-  onPressCity(item) {
-    console.log('onPressCity =', item);
-  }
-
-  renderItem(city) {
-    return (
-      <TouchableOpacity style={styles.item} onPress={() => this.onPressCity(city)}>
-        <Text style={styles.text}>{city}</Text>
-      </TouchableOpacity>
-    );
-  }
 
   render() {
     return (
-      <FlatList style={styles.container}
-                renderItem={({ item }) => this.renderItem(item)}
-                keyExtractor={item => item}
-                data={this.state.cities}
-      />
+      <CityList/>
     );
   }
+
 }
 
 const styles = StyleSheet.create({
