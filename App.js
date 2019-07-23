51e28de2e9971345ccc65125aea28a13f143d2cb
sooyeon <tndus130@ajou.ac.kr>
2019-07-23 14:56:51 +0900

    Add detail screen

--- App.js
+++ App.js
@@ -1,33 +1,16 @@
 import React from 'react';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import CityList from './CityList';
+import WeatherDetailScreen from './WeatherDetailScreen';
 
-
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: CityList
+const AppNavigator = createStackNavigator(
+  {
+    CityList: CityList,
+    Detail: WeatherDetailScreen,
+  },
+  {
+    initialRouteName: 'CityList',
   }
-});
+);
 
 export default createAppContainer(AppNavigator);
-/*
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    marginTop: Constants.statusBarHeight,
-  },
-
-  item: {
-    flex: 1,
-    height: 50,
-    justifyContent: 'center',
-
-    borderWidth: 1,
-    borderColor: 'orange',
-  },
-  text: {
-    fontSize: 20,
-    textAlign: 'center',
-  }
-});*/
